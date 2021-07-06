b03044c19504faee0cb6c712fee8e7f38b57a28c
alenafedyaeva <291407@gmail.com>
2021-07-06 17:57:09 +0300

    rw add

---
+++ homework1/settings/rwsettings.go
@@ -0,0 +1,17 @@
+package settings
+
+func readTOMSSettings(fname string) {
+
+}
+
+func readJSONSettings(fname string) {
+
+}
+
+func writeTOMLSettings(settings *Settings) error {
+
+}
+
+func writeJSONSettings(settings *Settings) error {
+
+}
